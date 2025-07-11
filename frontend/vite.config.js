842349c2395e0cdd8d725671751dff4e83186cd1
CryptoThibault <thibault.chalaoux@gmail.com>
2025-07-11 09:30:31 +0200

    certifs and ws

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,22 @@
+import fs from 'fs';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
+    https: {
+      key: fs.readFileSync('/app/certs/localhost-key.pem'),
+      cert: fs.readFileSync('/app/certs/localhost.pem'),
+    },
     port: 3000,
     open: false,
     strictPort: true,
     host: "0.0.0.0",
+    hmr: {
+      protocol: 'wss',
+      host: 'localhost',
+      port: 8443,
+      path: '/ws/',
+    },
     watch: {
       usePolling: true
     },
